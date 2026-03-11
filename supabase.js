675724fea46052c7f9a34debc22aece7257ebe21
Saxpriest <131813587+Saxpriest@users.noreply.github.com>
2026-03-11 04:50:10 +0100

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,12 @@
 <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
 <script>
-  // Supabase configuration
-  const SUPABASE_URL = "https://ipecphnepjvaogyxxqfh.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZWNwaG5lcGp2YW9neXh4cWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MzA1MTIsImV4cCI6MjA3NTAwNjUxMn0.D9ySAcvy6V_Mf2HxHjGpLzbyespaPB0ckS2ZgvdwtYc";
+const SUPABASE_URL = "https://ogzzwegtifhtjygnsuxr.supabase.co";
 
-  // Create a global Supabase client
-  window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nenp3ZWd0aWZodGp5Z25zdXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxOTEwMzIsImV4cCI6MjA4ODc2NzAzMn0.Hp7F2GDqfBeHxXKek3BkN_P1HAuqhab5jEogbaKygB8";
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 </script>
